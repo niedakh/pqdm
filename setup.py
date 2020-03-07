c576725bb9feaee2afe72186f27c0fa2aa257032
Piotr Szymanski <niedakh@gmail.com>
2020-03-07 16:34:45 +0100

    drop python 3.5

--- setup.py
+++ setup.py
@@ -21,14 +21,13 @@ test_requirements = ['pytest>=3', ]
 setup(
     author="Piotr Szymański",
     author_email='niedakh@gmail.com',
-    python_requires='>=3.5',
+    python_requires='>=3.6',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
