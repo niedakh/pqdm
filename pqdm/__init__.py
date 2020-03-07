e4791929f11f68bd540fa2064ebb06f62dd3c455
Piotr Szymanski <niedakh@gmail.com>
2020-03-07 20:40:39 +0100

    Bump version: 0.0.1 → 0.1.0

--- pqdm/__init__.py
+++ pqdm/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Piotr Szymański"""
 __email__ = 'niedakh@gmail.com'
-__version__ = '0.0.1'
+__version__ = '0.1.0'
--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/niedakh/pqdm',
-    version='0.0.1',
+    version='0.1.0',
     zip_safe=False,
 )
