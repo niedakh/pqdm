c0c22e67957c8e2a4096b5deb466f0f891f94d8d
Taher Chegini <taher.chegini@gmail.com>
2020-05-03 20:07:06 -0500

    the new notebook detetion method added
    There's no need for this function with my latest PR

--- pqdm/utils.py
+++ pqdm/utils.py
@@ -5,22 +5,6 @@ from typing import Dict, Any, Tuple
 
 KwArgs = Dict[str, Any]
 
-
-def _inside_jupyter() -> bool:
-    # an approach from stackoverflow, has a couple of problems
-    # doesn't handle 'jupyter console'
-    try:
-        shell = get_ipython().__class__.__name__
-        if shell == 'ZMQInteractiveShell':
-            return True  # Jupyter notebook or qtconsole
-        elif shell == 'TerminalInteractiveShell':
-            return False  # Terminal running IPython
-        else:
-            return False  # Other type (?)
-    except NameError:
-        return False
-
-
 def _divide_kwargs(kwargs: KwArgs, executor_class: Executor) -> Tuple[KwArgs, KwArgs]:
     executor_args = {
         k: kwargs[k] for k in inspect.getfullargspec(executor_class)[0]
