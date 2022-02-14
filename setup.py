58188e55455fce1f70030acf5460b51d988f33e5
Piotr Szymański <niedakh@gmail.com>
2022-02-14 11:13:23 +0100

    add long_description_content_type

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
     install_requires=requirements,
     license="MIT license",
     long_description=readme + '\n\n' + history,
+    long_description_content_type='text/x-rst',
     include_package_data=True,
     keywords='pqdm',
     name='pqdm',
